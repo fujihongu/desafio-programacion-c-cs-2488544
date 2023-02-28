30932477c90783047dac16a02ba49049f2e49dab
Flaviu Emanuel Hongu <125879277+fujihongu@users.noreply.github.com>
2023-02-28 17:06:30 +0000

    Finished command mockup

--- src/challenge.c
+++ src/challenge.c
@@ -6,6 +6,7 @@ Platform: LinkedIn Learning, thanks to Fujitsu*/
 #include <string.h>
 #include <time.h>
 
+static const char version_text[50] = "Error Logger C Challenge Version 0";
 char current_date[15];
 unsigned short int op_id;
 
@@ -14,6 +15,9 @@ void get_date(void);
 void logger(void);
 void show_info(void);
 void show_menu(void);
+void show_end_info(void);
+int grab_command(void);
+int execute_command(int cmd_nbr);
 
 int main(void)
 {
@@ -21,7 +25,7 @@ int main(void)
   printf("Initializing logging manager.\nDetecting current date...\n");
   get_date();
   get_id();
-  printf("Hit a key to start.\n");
+  printf("Press enter to start.\n");
   getchar();
   system("clear");
   logger();
@@ -60,8 +64,10 @@ void logger(void)
   {
     show_info();
     show_menu();
-    system("clear");
+    exit = grab_command();
   }
+  system("clear");
+  show_end_info();
 }
 
 void show_menu(void)
@@ -72,12 +78,65 @@ void show_menu(void)
   printf("3. Delete today's error history\n");
   printf("0. Exit\n");
   printf("\nPrompt: ");
-  getchar();
   printf("\n");
 }
 
 void show_info(void)
 {
-  printf("Error Logger C Challenge Version 0.\n");
+  printf(version_text);
+  printf("\n");
   printf("Operator ID: %d\nDate: %s\n", op_id, current_date);
+}
+
+int grab_command(void)
+{
+  char command[15];
+  int cmd_nbr;
+
+  scanf("%s", command);
+  getchar();
+  cmd_nbr = atoi(command);
+  if (strlen(command) == 1 && *command == '0')
+    return (execute_command(0));
+  else if (cmd_nbr != 0)
+  {
+    return (execute_command(cmd_nbr));
+  }
+  else
+    printf("Invalid action number or command.\nPlease, input a valid action.\n");
+  return (0);
+}
+
+int execute_command(int cmd_nbr)
+{
+  switch (cmd_nbr)
+  {
+  case 1:
+    printf("You logged an error.\n");
+    break;
+  case 2:
+    printf("This is today's error history:\nnull\n");
+    break;
+  case 3:
+    printf("For some reason you deleted today's error history.\nMaybe I just misinterpreted the instructions?\n");
+    break;
+  case 0:
+    return (1);
+  default:
+    printf("Invalid action number or command.\nPlease, input a valid action.\n");
+  }
+  printf("Press enter to continue.\n");
+  getchar();
+  system("clear");
+  return (0);
+}
+
+void show_end_info(void)
+{
+  printf("Exiting session 'Flaviu Emanuel Hongu'...\n\n");
+  printf(version_text);
+  printf("\n");
+  printf("Press enter to exit program.");
+  getchar();
+  system("clear");
 }
