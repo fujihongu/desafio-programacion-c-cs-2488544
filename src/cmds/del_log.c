e461a37c2f769ecd0dbdcfa2294ac86857671c64
Flaviu Emanuel Hongu <125879277+fujihongu@users.noreply.github.com>
2023-03-03 11:52:23 +0000

    Fixed backup

--- src/cmds/del_log.c
+++ src/cmds/del_log.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "../../lib/del_log.h"
 
 void del_log(t_info info)
 {
-  char *logname;
+  FILE *logfile;
+  FILE *backup;
+  char *logname = malloc(sizeof(info.current_date) + 5);
+  char *backupname = malloc(sizeof(info.current_date) + 10);
+  char cp;
 
-  logname = strcat(info.current_date, ".log");
+  // Get file names
+  logname = strcpy(logname, info.current_date);
+  logname = strcat(logname, ".log");
+  backupname = strcpy(backupname, info.current_date);
+  backupname = strcat(backupname, ".log.bak");
+  // Open backup and log files
+  backup = fopen(backupname, "w+");
+  logfile = fopen(logname, "a+");
+  // Copy contents of logfile into backup, then close them both
+  cp = fgetc(logfile);
+  while (cp != EOF)
+  {
+    fputc(cp, backup);
+    cp = fgetc(logfile);
+  }
+  fclose(backup);
+  fclose(logfile);
   if (remove(logname) == 0)
   {
     printf("Log file deleted successfully.\n");
